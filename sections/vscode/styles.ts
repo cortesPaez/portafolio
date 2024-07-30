65f8cc4ab92204fbb970bfff62c8914e5f238d92
ccortesdev <cristobal.cortes@aeolabs.io>
2024-07-30 15:54:10 -0400

    fix: remove min width

--- sections/vscode/styles.ts
+++ sections/vscode/styles.ts
@@ -2,7 +2,6 @@ import styled from "styled-components";
 
 export const Screen = styled.div`
   min-height: 500px;
-  min-width: 1000px;
   position: relative;
   display: flex;
   font-family: "Segoe UI", Arial, Helvetica, sans-serif;
